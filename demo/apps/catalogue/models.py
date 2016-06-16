c3f92a86418448672911891a44d9dcd9c70a8db9
Coen van der Kamp <coen@fourdigits.nl>
2016-06-16 14:02:08 +0200

    Use property name title

--- demo/apps/catalogue/models.py
+++ demo/apps/catalogue/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
-
 from wagtail.wagtailcore.models import Page
 
 
@@ -9,10 +6,9 @@ class Category(Page):
     The Oscars Category as a Wagtail Page
     This works because they both use Treebeard
     """
-    name = models.CharField(
-        verbose_name=_('name'),
-        max_length=255,
-        help_text=_("Category name")
-    )
+
+    @property
+    def name(self):
+        return self.title
 
 from oscar.apps.catalogue.models import *  # noqa

929bb34da9307e7332cb0d683cd933d1f53b9e17
Peter Govers <petergovers@gmail.com>
2016-06-17 10:21:47 +0200

    ContentType import

--- demo/apps/catalogue/models.py
+++ demo/apps/catalogue/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
 
 from oscar.core.utils import slugify

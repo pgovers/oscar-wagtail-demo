2645251aee78ff775cbda1938e38643cc5b05962
Peter Govers <petergovers@gmail.com>
2016-06-17 15:04:42 +0200

    Quick-'n-dirty customization of CatalogueImporter _create_stockrecord method

--- demo/apps/partner/importers.py
+++ demo/apps/partner/importers.py
@@ -1,76 +1,14 @@
-import os
-from decimal import Decimal as D
+from oscar.apps.partner.importers import CatalogueImporter as BaseImporter
+from oscar.core.loading import get_classes
 
-from django.db.transaction import atomic
-from django.utils.translation import ugettext_lazy as _
+from demo.apps.catalogue.categories import create_from_breadcrumbs
+from demo.apps.catalogue.models import Category
 
-from oscar.apps.catalogue.categories import create_from_breadcrumbs
-from oscar.core.compat import UnicodeCSVReader
-from oscar.core.loading import get_class, get_classes
-
-ImportingError = get_class('partner.exceptions', 'ImportingError')
-Partner, StockRecord = get_classes('partner.models', ['Partner',
-                                                      'StockRecord'])
-ProductClass, Product, Category, ProductCategory = get_classes(
-    'catalogue.models', ('ProductClass', 'Product', 'Category',
-                         'ProductCategory'))
+ProductClass, Product, ProductCategory = get_classes(
+    'catalogue.models', ('ProductClass', 'Product', 'ProductCategory'))
 
 
-class CatalogueImporter(object):
-    """
-    CSV product importer used to built sandbox. Might not work very well
-    for anything else.
-    """
-
-    _flush = False
-
-    def __init__(self, logger, delimiter=",", flush=False):
-        self.logger = logger
-        self._delimiter = delimiter
-        self._flush = flush
-
-    def handle(self, file_path=None):
-        u"""Handles the actual import process"""
-        if not file_path:
-            raise ImportingError(_("No file path supplied"))
-        Validator().validate(file_path)
-        if self._flush is True:
-            self.logger.info(" - Flushing product data before import")
-            self._flush_product_data()
-        self._import(file_path)
-
-    def _flush_product_data(self):
-        u"""Flush out product and stock models"""
-        Product.objects.all().delete()
-        ProductClass.objects.all().delete()
-        Partner.objects.all().delete()
-        StockRecord.objects.all().delete()
-
-    @atomic
-    def _import(self, file_path):
-        u"""Imports given file"""
-        stats = {'new_items': 0,
-                 'updated_items': 0}
-        row_number = 0
-        with UnicodeCSVReader(
-                file_path, delimiter=self._delimiter,
-                quotechar='"', escapechar='\\') as reader:
-            for row in reader:
-                row_number += 1
-                self._import_row(row_number, row, stats)
-        msg = "New items: %d, updated items: %d" % (stats['new_items'],
-                                                    stats['updated_items'])
-        self.logger.info(msg)
-
-    def _import_row(self, row_number, row, stats):
-        if len(row) != 5 and len(row) != 9:
-            self.logger.error("Row number %d has an invalid number of fields"
-                              " (%d), skipping..." % (row_number, len(row)))
-            return
-        item = self._create_item(*row[:5], stats=stats)
-        if len(row) == 9:
-            # With stock data
-            self._create_stockrecord(item, *row[5:9], stats=stats)
+class CatalogueImporter(BaseImporter):
 
     def _create_item(self, product_class, category_str, upc, title,
                      description, stats):
@@ -94,50 +32,14 @@ class CatalogueImporter(object):
         item.save()
 
         # Category
-        cat = create_from_breadcrumbs(category_str)
-        ProductCategory.objects.create(product=item, category=cat)
+        category = create_from_breadcrumbs(category_str)
+        try:
+            ProductCategory.objects.create(product=item, category=category)
+        except ValueError:
+            object = Category.objects.filter(pk=category.pk).first()
+            if object:
+                ProductCategory.objects.get_or_create(
+                    product=item, category=object
+                )
 
         return item
-
-    def _create_stockrecord(self, item, partner_name, partner_sku,
-                            price_excl_tax, num_in_stock, stats):
-        # Create partner and stock record
-        partner, _ = Partner.objects.get_or_create(
-            name=partner_name)
-        try:
-            stock = StockRecord.objects.get(partner_sku=partner_sku)
-        except StockRecord.DoesNotExist:
-            stock = StockRecord()
-
-        stock.product = item
-        stock.partner = partner
-        stock.partner_sku = partner_sku
-        stock.price_excl_tax = D(price_excl_tax)
-        stock.num_in_stock = num_in_stock
-        stock.save()
-
-
-class Validator(object):
-
-    def validate(self, file_path):
-        self._exists(file_path)
-        self._is_file(file_path)
-        self._is_readable(file_path)
-
-    def _exists(self, file_path):
-        u"""Check whether a file exists"""
-        if not os.path.exists(file_path):
-            raise ImportingError(_("%s does not exist") % (file_path))
-
-    def _is_file(self, file_path):
-        u"""Check whether file is actually a file type"""
-        if not os.path.isfile(file_path):
-            raise ImportingError(_("%s is not a file") % (file_path))
-
-    def _is_readable(self, file_path):
-        u"""Check file is readable"""
-        try:
-            f = open(file_path, 'r')
-            f.close()
-        except IOError:
-            raise ImportingError(_("%s is not readable") % (file_path))

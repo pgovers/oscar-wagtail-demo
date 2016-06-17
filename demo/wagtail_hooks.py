5d02064855e58c0cc1466baa8fd138296bacd4b4
Peter Govers <petergovers@gmail.com>
2016-06-17 15:58:51 +0200

    Extended Wagtail admin w/ link to Oscar dashboard

--- demo/wagtail_hooks.py
+++ demo/wagtail_hooks.py
@@ -1,8 +1,22 @@
 from django.utils.html import format_html
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 from wagtail.wagtailcore import hooks
+from wagtail.wagtailadmin.menu import MenuItem
+
 
 @hooks.register('insert_editor_css')
 def editor_css():
-    return format_html('<link rel="stylesheet" href="' + settings.STATIC_URL + 'demo/css/admin-streamfield-styles.css">')
+    return format_html(
+        '<link rel="stylesheet" href="%(static_url)sdemo/css/'
+        'admin-streamfield-styles.css">' % {'static_url': settings.STATIC_URL}
+    )
+
+
+@hooks.register('register_admin_menu_item')
+def register_oscar_menu_item():
+    return MenuItem(
+        'Oscar dashboard', reverse('dashboard:index'),
+        classnames='icon icon-redirect', order=10000
+    )
